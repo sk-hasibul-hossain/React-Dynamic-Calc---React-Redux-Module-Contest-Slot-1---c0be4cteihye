78d5c0e79fbd952177e6dfbdcd4f5397076f0762
SK Hasibul Hossain <86801974+sk-hasibul-hossain@users.noreply.github.com>
2023-12-17 09:52:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,31 @@ import '../styles/App.css';
 const App = () => {
 
   return (
+    const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  return (
     <div id="main">
-      <input id='input1' onChange={}/>
+      <input id='input1' onChange={(e) => {
+        const num = parseInt(e.target.value);
+        if(num){
+          setNum1(num);
+        }else{
+          setNum1(0);
+        }
+      }}/>
        +
-      <input id='input2' onChange={}/> 
+      <input id='input2' onChange={
+        (e) => {
+          const num = parseInt(e.target.value);
+        if(num){
+          setNum2(num);
+        }else{
+          setNum2(0);
+        }
+        }
+      }/> 
       
-      <p id='result'></p>
+      <p id='result'>{num1+num2}</p>
     </div>
   )
 }
