417c9105cac8ef16f38114ca41faa248240fb7fb
SK Hasibul Hossain <86801974+sk-hasibul-hossain@users.noreply.github.com>
2023-12-17 09:55:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,36 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
-
-  return (
-    const [num1, setNum1] = useState(0);
+  const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
   return (
     <div id="main">
-      <input id='input1' onChange={(e) => {
-        const num = parseInt(e.target.value);
-        if(num){
-          setNum1(num);
-        }else{
-          setNum1(0);
-        }
-      }}/>
-       +
-      <input id='input2' onChange={
-        (e) => {
+      <input
+        id="input1"
+        onChange={(e) => {
           const num = parseInt(e.target.value);
-        if(num){
-          setNum2(num);
-        }else{
-          setNum2(0);
-        }
-        }
-      }/> 
-      
-      <p id='result'>{num1+num2}</p>
+          if (num) {
+            setNum1(num);
+          } else {
+            setNum1(0);
+          }
+        }}
+      />
+      +
+      <input
+        id="input2"
+        onChange={(e) => {
+          const num = parseInt(e.target.value);
+          if (num) {
+            setNum2(num);
+          } else {
+            setNum2(0);
+          }
+        }}
+      />
+      <p id="result">{num1 + num2}</p>
     </div>
-  )
+  );
 }
 
 
